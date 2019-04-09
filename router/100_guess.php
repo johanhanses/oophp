9cb3cf4f85a24260f850c8586a471db9f9246840
Johan Hanses <you@example.com>
2019-04-09 08:43:07 +0200

    task 3 of kmom02 completed

--- router/100_guess.php
+++ router/100_guess.php
@@ -54,6 +54,68 @@ $app->router->get("guess/play", function () use ($app) {
 });
 
 
+
+// /**
+//  * Play the game - make a guess
+//  */
+// $app->router->post("guess/play", function () use ($app) {
+//     // Assign incoming variables
+//     $guess    = $_POST["guess"] ?? null;
+//     $doInit   = $_POST["doInit"] ?? null;
+//     $doGuess  = $_POST["doGuess"] ?? null;
+//     $doCheat  = $_POST["doCheat"] ?? null;
+
+//     $number   = $_SESSION["number"] ?? null;
+//     $tries    = $_SESSION["tries"] ?? null;
+//     $res = null;
+
+//     if ($doInit || $number === null) {
+//     //Init the game
+//         $game = new PJH\Guess\Guess();
+//         $_SESSION["number"] = $game->number();
+//         $_SESSION["tries"] = $game->tries();
+//         return $app->response->redirect("guess/play");
+//     } 
+//     return $app->response->redirect("guess/play");
+// });   
+    
+    
+    
+    
+    
+//     elseif ($doGuess && $tries === 1) {
+//     //re init the game if out of guesses
+//         $game = new PJH\Guess\Guess();
+//         $res = $game->startOver();
+//         $_SESSION["number"] = $game->number();
+//         $_SESSION["tries"] = $game->tries();
+//         $_SESSION["res"] = $res;
+//         $_SESSION["guess"] = $guess;
+//     } elseif ($doGuess) {
+//     // do a guess
+//         $game = new PJH\Guess\Guess($number, $tries);
+//         $res = $game->makeGuess($guess);
+//         $_SESSION["tries"] = $game->tries();
+//         $_SESSION["res"] = $res;
+//         $_SESSION["guess"] = $guess;
+//     } elseif ($doCheat) {
+//     // cheat
+//         $game = new PJH\Guess\Guess($number, $tries);
+//         $res = $game->cheat();
+//         $_SESSION["res"] = $res;
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Play the game - make a guess
  */
