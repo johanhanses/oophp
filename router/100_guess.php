ef1332593960148db0e931e63a0ef238c2b54f02
Johan Hanses <you@example.com>
2019-04-28 22:19:06 +0200

    removed debugging transcript

--- router/100_guess.php
+++ router/100_guess.php
@@ -32,7 +32,7 @@ $app->router->get("guess/play", function () use ($app) {
     $number = $_SESSION["number"] ?? null;
     $tries = $_SESSION["tries"] ?? null;
     $res = $_SESSION["res"] ?? null;
-    
+
     $_SESSION["res"] = null;
     $_SESSION["guess"] = null;
 
@@ -46,62 +46,12 @@ $app->router->get("guess/play", function () use ($app) {
     ];
 
     $app->page->add("guess/play", $data);
-    $app->page->add("guess/debug");
+    // $app->page->add("guess/debug");
 
     return $app->page->render([
         "title" => $title,
     ]);
 });
-
-
-
-// /**
-//  * Play the game - make a guess
-//  */
-// $app->router->post("guess/play", function () use ($app) {
-//     // Assign incoming variables
-//     $guess    = $_POST["guess"] ?? null;
-//     $doInit   = $_POST["doInit"] ?? null;
-//     $doGuess  = $_POST["doGuess"] ?? null;
-//     $doCheat  = $_POST["doCheat"] ?? null;
-
-//     $number   = $_SESSION["number"] ?? null;
-//     $tries    = $_SESSION["tries"] ?? null;
-//     $res = null;
-
-//     if ($doInit || $number === null) {
-//     //Init the game
-//         $game = new PJH\Guess\Guess();
-//         $_SESSION["number"] = $game->number();
-//         $_SESSION["tries"] = $game->tries();
-//         return $app->response->redirect("guess/play");
-//     }
-//     return $app->response->redirect("guess/play");
-// });
-
-
-
-//     elseif ($doGuess && $tries === 1) {
-//     //re init the game if out of guesses
-//         $game = new PJH\Guess\Guess();
-//         $res = $game->startOver();
-//         $_SESSION["number"] = $game->number();
-//         $_SESSION["tries"] = $game->tries();
-//         $_SESSION["res"] = $res;
-//         $_SESSION["guess"] = $guess;
-//     } elseif ($doGuess) {
-//     // do a guess
-//         $game = new PJH\Guess\Guess($number, $tries);
-//         $res = $game->makeGuess($guess);
-//         $_SESSION["tries"] = $game->tries();
-//         $_SESSION["res"] = $res;
-//         $_SESSION["guess"] = $guess;
-//     } elseif ($doCheat) {
-//     // cheat
-//         $game = new PJH\Guess\Guess($number, $tries);
-//         $res = $game->cheat();
-//         $_SESSION["res"] = $res;
-//     }
 
 
 
